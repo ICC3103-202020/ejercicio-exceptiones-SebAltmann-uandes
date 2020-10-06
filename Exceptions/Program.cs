04e48d9afc6a3e016fc99e85b017aca41ba3895c
Seb Altmann <saaltmann@miuandes.cl>
2020-10-06 19:32:04 -0300

    Int to String exception example v0.1

---
+++ Exceptions/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        public static void IntCheckerForString(string x)
+        {
+            try
+            {
+                int i = Int32.Parse(x);
+                Console.WriteLine(i);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return;
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String to Int: ");
+            String a = Console.ReadLine();
+            IntCheckerForString(a);
+            Console.ReadKey();
+        }
+    }
+}
